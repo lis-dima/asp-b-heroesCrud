7775232275a4de93975640ab6e1a868188154ff8
lis-dima <lisiev.dmytro@gmail.com>
2023-01-02 21:46:05 +0000

    data from DB

--- asp-b-heroesCrud/Server/Controllers/SuperHeroController.cs
+++ asp-b-heroesCrud/Server/Controllers/SuperHeroController.cs
@@ -1,5 +1,7 @@
-﻿using asp_b_heroesCrud.Shared;
+﻿using asp_b_heroesCrud.Server.Data;
+using asp_b_heroesCrud.Shared;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace asp_b_heroesCrud.Server.Controllers
 {
@@ -7,33 +9,31 @@ namespace asp_b_heroesCrud.Server.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
-        public static List<Comic> comics = new List<Comic> {
-            new Comic {Id = 1, Name = "Marvel"},
-            new Comic {Id = 2, Name = "DC"}
-        };
+        public DataContext context { get; }
 
-        public static List<SuperHero> heroes = new List<SuperHero>
+        public SuperHeroController(DataContext dataContext)
         {
-            new SuperHero {Id = 1, FirstName = "Peter", LastName = "Parker", HeroName="SpiderM@n" },
-            new SuperHero {Id = 2, FirstName = "Bruce", LastName = "Wayne", HeroName="B@tm@n", Comic = comics[1], ComicId = 2 },
-        };
+            context = dataContext;
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
         {
-            return Ok(heroes);
+            return Ok(await context.SuperHeroes.ToListAsync());
         }
 
         [HttpGet("comics")]
         public async Task<ActionResult<List<SuperHero>>> GetComics()
         {
-            return Ok(comics);
+            return Ok(await context.Comics.ToListAsync());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<SuperHero>> GetSingleHero(int? id)
+        public async Task<ActionResult<SuperHero>> GetSingleHero(int id)
         {
-            var hero = heroes.FirstOrDefault(h => h.Id == id);
+            var hero = await context.SuperHeroes
+                .Include(h => h.Comic)
+                .FirstOrDefaultAsync(h => h.Id == id);
             if (hero == null)
                 return NotFound("Sorry :(");
             return Ok(hero);
